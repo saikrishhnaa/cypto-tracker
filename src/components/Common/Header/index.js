27dfeeca38311f812513257cac7fad2de5b6a81d
saikrishhnaa <saikrishnaamakam@gmail.com>
2024-08-05 01:16:59 +0530

    Done with setup and started with landing page

---
+++ src/components/Common/Header/index.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import "./styles.css";
+import TemporaryDrawer from './drawer';
+import Button from '../Button';
+
+function Header() {
+  return (
+    <div className="navbar">
+      <h1 className='logo'>
+        CyptoTracker<span style={{color: "var(--blue)"}}>.</span>
+      </h1>
+      <div className="links">
+        <a href="/">
+          <p className='link'>Home</p>
+        </a>
+        <a href="/">
+          <p className='link'>Compare</p>
+        </a>
+        <a href="/">
+          <p className='link'>Watchlist</p>
+        </a>
+        <a href="#">
+          <Button text={"Dashboard"} outlined onClick={()=>console.log("btn clicked")}/>
+        </a>
+      </div>
+      <div className="mobile-drawer">
+        <TemporaryDrawer/>
+      </div>
+    </div>
+  );
+}
+
+export default Header
